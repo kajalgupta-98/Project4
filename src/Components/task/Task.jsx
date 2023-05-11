af8c1b101da914a3fbbe3751e85783551f360d54
chand babu <jafri.nbj@gmail.com>
2023-05-11 15:29:44 +0530

    changes some css

--- src/Components/task/Task.jsx
+++ src/Components/task/Task.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from 'react'
 import style from './Task.module.css'
 import ModeEditOutlinedIcon from '@mui/icons-material/ModeEditOutlined';
+import DialogBox from '../Dialog/DialogBox';
 
 function Task(props) {
-    const [visibility, setVisibility] = useState(false)
+    const [visibility, setVisibility] = useState(false);
+  const [open, setOpen] = React.useState(false);
+
 
     const handleClick = ()=>{
-        alert('Hello')
+        setOpen(!open);
     }
     
   return (
     <div className={style.task_box} onClick={handleClick} onMouseOver={()=>setVisibility(true)} onMouseOut={()=>setVisibility(false)}>
-      <span className={style.task} >{props.task}</span>
+      <span className={style.task} >{props.task||"hello"}</span>
 
       <div className={visibility?style.pencil:style.hiddenPencil} >
       <ModeEditOutlinedIcon/>
       </div>
+      {
+      open&&<DialogBox setOpen={setOpen} open={open} />
+    }
     </div>
+    
   )
 }
 
