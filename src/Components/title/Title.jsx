632091ff99d8db4549d3ab6508b4816e94e85762
Rohit-2012 <rohit.kirti1219@gmail.com>
2023-05-11 13:06:48 +0530

    completed task component

--- src/Components/title/Title.jsx
+++ src/Components/title/Title.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from './Title.module.css'
+import VideoLabelIcon from '@mui/icons-material/VideoLabel';
 
 function Title() {
+  const [toggleInput, setToggleInput] = useState(true)
+  const [title, setTitle] = useState('')
+
   return (
-    <div>
-      <h1>Title</h1>
+    <div className={style.title_div}>
+      <VideoLabelIcon fontSize='small'/>
+      <div className={style.title_text}>
+      <input type="text" value={title} onClick={()=>setToggleInput(true)} onChange={(e)=>setTitle(e.target.value)} className={style.title_box}/>
+    <span>in list Todo</span>
+      </div>
     </div>
+
   )
 }
 
--- src/Containers/board/Board.jsx
+++ src/Containers/board/Board.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Card from '../../Components/card/Card'
+import Title from '../../Components/title/Title'
 
 function Board() {
   return (
     <div>
       <Card/>
+      <Title/>
     </div>
   )
 }
