5d41036e0042729439d250b5e927bfdd2776bf15
kajalgupta-98 <122223292+kajalgupta-98@users.noreply.github.com>
2023-05-11 23:53:11 +0530

    description component done

--- src/Components/description/Description.jsx
+++ src/Components/description/Description.jsx
@@ -2,18 +2,14 @@ import { useState } from "react";
 import style from "./description.module.css";
 import { CgMenuLeftAlt } from "react-icons/cg";
 import { TfiAnnouncement } from "react-icons/tfi";
-import {AiOutlineInfoCircle} from "react-icons/ai"
+import { AiOutlineInfoCircle } from "react-icons/ai"
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-// import MUIRichTextEditor from "mui-rte";
-// import { createTheme, ThemeProvider } from "@mui/material/styles";
-// import { Editor } from "@mui/lab";
+
 
 const Description = () => {
   const [showaddDescriptionBox, setShowaddDescriptionBox] = useState(false);
   const [value, setValue] = useState("");
-
-  // const myTheme = createTheme({});
 
   function handleShowAddDescriptionBox() {
     setShowaddDescriptionBox(!showaddDescriptionBox);
@@ -23,24 +19,16 @@ const Description = () => {
       <div className={style.header}>
         <CgMenuLeftAlt />
         <h3>Description</h3>
-        {showaddDescriptionBox &&  <p><AiOutlineInfoCircle/></p>}
+        {showaddDescriptionBox && <p><AiOutlineInfoCircle /></p>}
       </div>
 
       <div className={style.content}>
         {showaddDescriptionBox ? (
           <div style={{ display: "flex", flexDirection: "column" }}>
             <div className={style.addDescriptionBox}>
-            <ReactQuill theme="snow" value={value} onChange={setValue} />;
-
-              {/* <ThemeProvider theme={myTheme}>
-                <MUIRichTextEditor label="Make your description even better..." />
-              </ThemeProvider> */}
-              {/* <Editor
-                  defaultValue="<p>Start typing here...</p>"
-                  toolbarPresent
-                  toolbarMode="wrap"
-                /> */}
+              <ReactQuill theme="snow" value={value} onChange={setValue} />;
             </div>
+
             <div className={style.bottomSection}>
               <div className={style.btnContainer}>
                 <button
@@ -56,9 +44,10 @@ const Description = () => {
                   Cancel
                 </button>
               </div>
+
               <span>
-              <TfiAnnouncement color="blue"/>
-              <p>Share Feedback</p>
+                <TfiAnnouncement color="blue" />
+                <p>Share Feedback</p>
               </span>
             </div>
           </div>
