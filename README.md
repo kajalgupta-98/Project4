d50d01917e59aba2c241ce40d29163640650924f
kajalgupta-98 <122223292+kajalgupta-98@users.noreply.github.com>
2023-05-11 10:36:58 +0530

    static sidebar

--- README.md
+++ README.md
@@ -6,3 +6,47 @@
 [-] Create side bar 
 [-] Create home section
 
+
+board = [
+    list : {}
+]
+
+list = {
+    list_id:"random id",
+    list_title:"todo/in-progress/done",
+    list_createdAt: "current date and time",
+    card: [
+        {
+        id: "id",
+        createdAt: "current date and time",
+        title:"task title",
+        description:"task description"
+        }
+    ]
+}
+
+
+
+list_id:[{
+    label: "Math.random()",
+    type: "number",
+    default: "null"
+}]
+
+list_title:[{
+    label: "todo/in-progress/done",
+    type: "String",
+    default: ""
+}]
+
+card :{
+    label: "card",
+    type: "array",
+}
+
+
+
+
+
+
+
--- src/Containers/sidebar/Sidebar.jsx
+++ src/Containers/sidebar/Sidebar.jsx
@@ -1,10 +1,80 @@
+// import React from 'react'
+import style from "./Sidebar.module.css"
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import TableChartOutlinedIcon from '@mui/icons-material/TableChartOutlined';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+// import Divider from '@mui/material/Divider';
 
-import React from 'react'
 
 function Sidebar() {
   return (
-    <div>
-      this is for Sidebar....
+    <div className={style.sideBarContainer}>
+      <div className={style.header}>
+        <button className={style.logoBtn}>T</button>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h4> Trello Workspace</h4>
+          <p>Free</p>
+        </div>
+        <NavigateBeforeIcon sx={{ fontSize: "1.2rem" }}/>
+      </div>
+
+      <hr style={{ width: "100%", height: "0.5px" }} />
+
+      <div className={style.sideBarOptions}>
+        <div className={style.optionInner}>
+          <TableChartOutlinedIcon sx={{ fontSize: "1.2rem" }} />
+          <p>Boards</p>
+        </div>
+      </div>
+
+      <div className={style.sideBarOptions}>
+        <div className={style.optionInner}>
+          <PersonOutlineOutlinedIcon sx={{ fontSize: "1.2rem" }} />
+          <p>Members</p>
+        </div>
+        <AddOutlinedIcon sx={{ fontSize: "1.2rem" }} />
+      </div>
+
+      <div className={style.sideBarOptions}>
+        <div className={style.optionInner}>
+          <SettingsOutlinedIcon sx={{ fontSize: "1.2rem" }} />
+          <p>Workspace Settings</p>
+        </div>
+        <ExpandMoreIcon sx={{ fontSize: "1.2rem" }} />
+      </div>
+
+      <h4 className={style.sideBarOptions}>WorkSpace Views</h4>
+
+      <div className={style.sideBarOptions}>
+        <div className={style.optionInner}>
+          <TableChartOutlinedIcon sx={{ fontSize: "1.2rem" }} />
+          <em> Table</em>
+        </div>
+        <MoreHorizOutlinedIcon sx={{ fontSize: "1.2rem" }} />
+      </div>
+
+      <div className={style.sideBarOptions}>
+        <div className={style.optionInner}>
+          <CalendarMonthOutlinedIcon sx={{ fontSize: "1.2rem" }} />
+          <em> Calendar</em>
+        </div>
+        <MoreHorizOutlinedIcon sx={{ fontSize: "1.2rem" }} />
+      </div>
+
+      <div className={style.sideBarOptions}>
+        <h4>Your Boards</h4>
+        <div className={style.optionInner}>
+          <MoreHorizOutlinedIcon sx={{ fontSize: "1.2rem" }} />
+          <AddOutlinedIcon sx={{ fontSize: "1.2rem" }} />
+        </div>
+      </div>
+
+
     </div>
   )
 }
